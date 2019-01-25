9b76d95840e847e7a877a51bcadb62203557d60a
Flavien Medina <flavien_94@hotmail.fr>
2019-01-25 15:12:21 +0100

    add orm.config.json file

--- src/mOrm.js
+++ src/mOrm.js
@@ -1,57 +1,47 @@
-import {
-    isEmpty
-} from 'lodash';
+import { isEmpty } from 'lodash';
 import PostgreSQL from './engine/postgresql';
 
 export default class mOrm {
-    async createConnection(dbConfig = {}) {
-        console.log(dbConfig.entities);
-        dbConfig.entities.forEach((entity) => {
+  async createConnection(dbConfig = {}) {
+    dbConfig.entities.forEach(entity => {});
 
-        })
-
-        if (typeof dbConfig === 'string') {
-            const arrConnection = dbConfig.match(/(.*):\/\/(.*):(.*)@(.*):(.*)\/(.*)/)
-            this.config = {
-                type: arrConnection[1],
-                username: arrConnection[2],
-                password: arrConnection[3],
-                host: arrConnection[4],
-                port: arrConnection[5],
-                database: arrConnection[6],
-            }
-        } else {
-            if (isEmpty(dbConfig)) {
-                if (!existsSync()) {
-                    throw new Error('NO CONFIG');
-                }
-                this.config = require(this);
-            } else {
-                this.config = dbConfig;
-            }
+    if (typeof dbConfig === 'string') {
+      const arrConnection = dbConfig.match(
+        /(.*):\/\/(.*):(.*)@(.*):(.*)\/(.*)/
+      );
+      this.config = {
+        type: arrConnection[1],
+        username: arrConnection[2],
+        password: arrConnection[3],
+        host: arrConnection[4],
+        port: arrConnection[5],
+        database: arrConnection[6]
+      };
+    } else {
+      if (isEmpty(dbConfig)) {
+        if (!existsSync()) {
+          throw new Error('NO CONFIG');
         }
-        const {
-            type,
-            host,
-            port,
-            username,
-            password,
-            database
-        } = this.config;
-        switch (type) {
-            case 'postgres':
-                this.dbInstance = new PostgreSQL({
-                    host,
-                    port,
-                    username,
-                    password,
-                    database
-                });
-                break;
-
-            default:
-                break;
-        }
-        await this.dbInstance.initialize();
+        this.config = require(this);
+      } else {
+        this.config = dbConfig;
+      }
     }
+    const { type, host, port, username, password, database } = this.config;
+    switch (type) {
+      case 'postgres':
+        this.dbInstance = new PostgreSQL({
+          host,
+          port,
+          username,
+          password,
+          database
+        });
+        break;
+
+      default:
+        break;
+    }
+    await this.dbInstance.initialize();
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,33 +1,21 @@
 import mOrm from './mOrm';
-import Student from './entities/student'
-import Project from './entities/project'
-import Note from './entities/note'
-// Let's Rock!
+import Student from './entities/student';
+import Project from './entities/project';
+import Note from './entities/note';
+
+import config from './../orm.config.json';
+
 (async () => {
-    const orm = new mOrm();
+  const orm = new mOrm();
 
-    try {
-        await orm.createConnection({
-
-            type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'madjid',
-            password: '',
-            database: 'mOrm',
-
-            synchronize: false,
-            entities: [
-                Student,
-                Project,
-                Note
-            ]
-
-        });
-
-
-    } catch (err) {
-        console.log(err);
-        process.exit(-1);
-    }
+  try {
+    await orm.createConnection({
+      ...config,
+      synchronize: false,
+      entities: [Student, Project, Note]
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(-1);
+  }
 })();
