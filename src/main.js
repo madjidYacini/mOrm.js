e3cbac9682b9fab1ae77c5494e5bb33fbc2b6560
madjid yacini <madjid.yacini@gmail.com>
2019-01-25 10:52:03 +0100

    init files

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import mOrm from './mOrm'
+
+// Let's Rock!
+;
+(async () => {
+    console.log('ça marche ');
+
+    const orm = new mOrm()
+
+    try {
+        await orm.createConnection({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'majdi',
+            password: 'toumi',
+            database: 'iLovePragmatic',
+        })
+
+        // or:
+        // orm.createConnection('postgresql://majdi:toumi@localhost:5432/iLovePragmatic')
+    } catch (err) {
+        console.log(err)
+        process.exit(-1)
+    }
+})()
